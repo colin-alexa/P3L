953e12b19277a16b98c8e0dd7b295cc12295f324
colinarobinson <colinarobinson@gmail.com>
2013-07-23 22:18:50 -0400

    Update dcavarCorpus.py
    updated to most recent version of corpus.py

--- dcavarCorpus.py
+++ dcavarCorpus.py
@@ -2,6 +2,7 @@
 # -*- coding: utf-8 -*-
 
 import re
+from operator import itemgetter
 
 
 def getTextFromFile(filename):
@@ -26,20 +27,49 @@ def tokenize(sometext):
    """Returns a list of tokens generated using re.split() and
    the regular expression (\W)"""
 
-   return re.split("(\W)", sometext)
+   # return re.split("(\W)", sometext)
+   return re.split("\W", sometext)
+
+
+def relativizeFP(fp):
+   total = sum(fp.values())
+   for key in fp:
+      fp[key] = fp[key] / total
+
+
+def getNGramModel(tokens, n):
+   mydict = {}
+   for x in range( len(tokens) - (n - 1) ):
+      ngram = " ".join( tokens[ x : x + n ] )
+      mydict[ngram] = mydict.get(ngram, 0) + 1
+   #relativizeFP(mydict)
+   return mydict
+
+
 
 
 
 def makeFrequencyProfile(tokenlist):
-   """ """
+   """Count elements in the tokenlist"""
    # count tokens and generate a frequency profile
    mydict = {}
    for token in tokenlist:
-      if token in mydict:
-         mydict[token] = mydict[token] + 1
-      else:
-         mydict[token] = 1
+      #if token in mydict:
+      #   mydict[token] = mydict[token] + 1
+      #else:
+      #   mydict[token] = 1
+      mydict[token] = mydict.get(token, 0) + 1
    return mydict
+
+
+def prettyPrintFRP(fp, byfrequency=True, myreverse=False):
+   if byfrequency:
+      myitems = sorted( fp.items(), key=itemgetter(1), reverse=myreverse )
+   else:
+      myitems = sorted( fp.items(), key=itemgetter(0), reverse=myreverse )
+   for x in myitems:
+      print(x[0], x[1], sep="\t")
+
 
 
 
